967b14e0cf486fac062af0075ea47d9389c8d9e7
Ali Bohlooli <bohlooli1999@gmail.com>
2021-06-29 21:11:25 +0430

    added js file

---
+++ myJs.js
@@ -0,0 +1,15 @@
+let farmArea = document.getElementById('farmArea');
+
+let plantNumber = document.getElementById('plantNumber');
+
+let costPer = document.getElementById('costPer');
+
+let interstRatio = document.getElementById('interstRatio');
+
+let perfomRatio = document.getElementById('perfomRatio');
+
+let answer = document.getElementById("Answer");
+
+function calc(){
+    answer =  farmArea + plantNumber + costPer + interstRatio + perfomRatio;
+}
