9deab7fefb4e31e3d4900b1d3428b657b06d2715
Ehsan <e.sajadi1379@gmail.com>
2021-06-30 19:38:03 +0430

    improve design

--- myJs.js
+++ myJs.js
@@ -8,9 +8,9 @@ function calc(){
     let plantNumber = document.getElementById('plantNumber').value;
     let costPer = document.getElementById('costPer').value;
     let interstRatio = document.getElementById('interstRatio').value;
-    // let performRatio = document.getElementById('performRatioRatio').value;
+    let performRatio = document.getElementById('performRatio').value;
 
     console.log(costPer)
 
-    answer.innerHTML = "نتیجه: " + (parseInt(farmArea) + parseInt(plantNumber) +parseInt(costPer)  + parseInt(interstRatio) ) ;
+    answer.innerHTML = "نتیجه: " + (parseInt(farmArea) + parseInt(plantNumber) +parseInt(costPer)  + parseInt(interstRatio) + parseInt(performRatio)) ;
 }
