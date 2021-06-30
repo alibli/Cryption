b62a9c410a09f291bf11de0237bcee2fbff9d4f1
Ehsan <e.sajadi1379@gmail.com>
2021-06-30 19:20:44 +0430

    js file fixed

--- myJs.js
+++ myJs.js
@@ -1,15 +1,16 @@
-let farmArea = document.getElementById('farmArea');
 
-let plantNumber = document.getElementById('plantNumber');
+let answer = document.getElementById("Answerz");
 
-let costPer = document.getElementById('costPer');
-
-let interstRatio = document.getElementById('interstRatio');
-
-let perfomRatio = document.getElementById('perfomRatio');
-
-let answer = document.getElementById("Answer");
 
 function calc(){
-    answer =  farmArea + plantNumber + costPer + interstRatio + perfomRatio;
+
+    let farmArea = document.getElementById('farmArea').value;
+    let plantNumber = document.getElementById('plantNumber').value;
+    let costPer = document.getElementById('costPer').value;
+    let interstRatio = document.getElementById('interstRatio').value;
+    // let performRatio = document.getElementById('performRatioRatio').value;
+
+    console.log(costPer)
+
+    answer.innerHTML =  parseInt(farmArea) + parseInt(plantNumber) +parseInt(costPer)  + parseInt(interstRatio) ;
 }
