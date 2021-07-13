159daade8b6ca6ea4a907875ab07b40530f1b1a5
Ehsan <e.sajadi1379@gmail.com>
2021-07-13 13:53:33 +0430

    js updated

--- myJs.js
+++ myJs.js
@@ -1,7 +1,4 @@
 
-let answer = document.getElementById("Answerz");
-
-
 function calc(){
 
     let farmArea = document.getElementById('farmArea').value;
@@ -9,6 +6,7 @@ function calc(){
     let costPer = document.getElementById('costPer').value;
     let interstRatio = document.getElementById('interstRatio').value;
     let performRatio = document.getElementById('performRatio').value;
+    let answer = document.getElementById("Answerz");
 
     console.log(costPer)
 
@@ -16,6 +14,60 @@ function calc(){
 
     console.log(answer)
     console.log(isNaN(answer.innerHTML))
+
+    if (isNaN(answer.innerHTML)){
+        answer.innerHTML = "لطفا تمام فیلد ها را پر کنید";
+        answer.style.color = 'darkred'
+    }else{
+        answer.innerHTML =  "نتیجه: "+ answer.innerHTML
+        answer.style.color = 'white'
+    }
+}
+
+function calc2(){
+
+    let farmprepare = document.getElementById('farmprepare').value;
+    let farmrent = document.getElementById('farmrent').value;
+    let plant = document.getElementById('plant').value;
+    let grow = document.getElementById('grow').value;
+    let harvest = document.getElementById('harvest').value;
+    let answer = document.getElementById("Answer2");
+
+
+    answer.innerHTML =  (parseInt(farmprepare) + parseInt(farmrent) +parseInt(plant)  + parseInt(grow) + parseInt(harvest)) ;
+
+    console.log(answer)
+    console.log(isNaN(answer.innerHTML))
+
+    if (isNaN(answer.innerHTML)){
+        answer.innerHTML = "لطفا تمام فیلد ها را پر کنید";
+        answer.style.color = 'darkred'
+    }else{
+        answer.innerHTML =  "نتیجه: "+ answer.innerHTML
+        answer.style.color = 'white'
+    }
+}
+
+function calc3(){
+
+    let farm = document.getElementById('farm').value;
+    let areaconstruction = document.getElementById('areaconstruction').value;
+    let building = document.getElementById('building').value;
+    let machinery = document.getElementById('machinery').value;
+    let equipment = document.getElementById('equipment').value;
+    let transportation = document.getElementById("transportation").value;
+    let officeequipment = document.getElementById("officeequipment").value;
+    let costbefore = document.getElementById("costbefore").value;
+    let unpredictedcosts = document.getElementById("unpredictedcosts").value;
+
+    let answer = document.getElementById("Answer3");
+
+    answer.innerHTML =  (parseInt(farm) + parseInt(areaconstruction) +parseInt(building)  + parseInt(machinery) + parseInt(equipment) + parseInt(transportation) +
+                            parseInt(officeequipment) + parseInt(costbefore) + parseInt(unpredictedcosts)) ;
+
+    console.log(answer)
+    console.log(isNaN(answer.innerHTML))
+
 
     if (isNaN(answer.innerHTML)){
         answer.innerHTML = "لطفا تمام فیلد ها را پر کنید";
